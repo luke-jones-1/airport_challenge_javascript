1d3a3a9a00fc15e34243574c5894bec360c94945
Luke Jones <lukejonesice@yahoo.co.uk>
2019-11-08 10:48:02 +0000

    final setup

--- karma.conf.js
+++ karma.conf.js
@@ -4,6 +4,8 @@
 module.exports = function(config) {
   config.set({
 
+    failOnEmptyTestSuite: false,
+
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
